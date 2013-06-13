6a705e08f5898dc357f9d229f73dab21fcc4296f
Tim Clipsham <tim.clipsham@gmail.com>
2013-06-13 18:56:21 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamsungSmarttvServer::Application.config.secret_key_base = 'cfe588d212be7d8d92b6a0d8181c6a32add9ecadbb40aacf7e3c5813276608c072be721cb249df71827b72fe66b875775acdbbae2153f4f0a00d764da1c7efbf'
