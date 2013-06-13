a2469788552edd037f561dfab8a6c49f8d9c3409
Tim Clipsham <tim.clipsham@gmail.com>
2013-06-14 01:08:54 +1000

    Improve styling and name widgetlist.xml route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SamsungSmarttvServer::Application.routes.draw do
   resources :widgets
 
-  get 'widgetlist.xml' => 'widgets#index', format: 'xml'
+  get 'widgetlist.xml' => 'widgets#index', :as => :widgetlistxml, :format => 'xml'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
